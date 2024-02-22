988c7a44c870a8f46638351a1b6b8e2f39e63638
Anlprnc <anilpirincciii@gmail.com>
2024-02-22 16:56:40 +0300

    sc

--- obj/Debug/net8.0/Asp.Net-E-Commerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/Asp.Net-E-Commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.Net-E-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c77a0ea825e1c7e4b4cc633d8f7137ba52a5214")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp.Net-E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp.Net-E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
